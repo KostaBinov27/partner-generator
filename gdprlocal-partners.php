3505ba21fd74014f4d1876896dbd4b3fedf150b8
kostabinov <kosta.b@influencers.club>
2026-02-23 22:28:49 +0100

    fixes

--- gdprlocal-partners.php
+++ gdprlocal-partners.php
@@ -7,7 +7,6 @@ Author: Kosta Binov
 */
 
 if (!defined('ABSPATH')) exit;
-include_once(ABSPATH . 'wp-admin/includes/plugin.php');
 
 /* =========================================================
    CONFIGURATION
